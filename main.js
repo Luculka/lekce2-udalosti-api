9f2422c3ddce96a58dcfd8c7b93d98a344ddcf73
Lucie Trojanova <trojanova33@gmail.com>
2021-03-20 09:03:18 +0100

    ukol final

--- main.js
+++ main.js
@@ -1,1 +1,52 @@
 // tady je místo pro náš program
+
+
+//bold po najetí myši na odstavec
+function mouseover(udalost) {
+    udalost.target.style.fontWeight = "bold";
+}
+
+let odstavec = document.querySelector(".odstavec");
+
+//červený text v odstavci po kliknutí na tlačítko
+function cerveny() {
+    odstavec.classList.toggle("cerveny");
+}
+
+//zvětšení písma
+function biggerText() {
+    odstavec.style.fontSize = "1.5rem";
+}
+
+// prehraj
+// @param {string} elementSelector;
+
+function prehraj(elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.play();
+}
+
+function zastav(elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.pause();
+}
+
+function vypnoutZvuk(elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.volume = 0.0;
+}
+
+function hlasitost1(elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.volume = 0.5;
+}
+
+function nejvyssiHlasitost(elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.volume = 1;
+}
+
+function naZacatek(elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.currentTime = 0.0;
+}
